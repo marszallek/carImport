b9b81e6ed16096c63826ba4b6875948cbe5728a4
marszallek <marszallek@vp.pl>
2019-03-17 23:11:32 +0100

    calculator on Vue and styling Bootstrap

--- js/vueapp.js
+++ js/vueapp.js
@@ -1,10 +1,10 @@
 const app = new Vue({
     el: '#app',
     data: {
-        currencyRate: 3.5,
+        currencyRate: 3.8,
         price: 1000,
         marketPrice: 20000,
-        placeOfOrigin: 'Transport do Nowego Yorku',
+        placeOfOrigin: 800,
     },
     computed: {
         copartCharges: function () {
@@ -17,7 +17,11 @@ const app = new Vue({
             return Math.round(this.marketPrice * 0.6 * 0.186)
         },
         priceToNewYork() {
-            return this.placeOfOrigin;
+            if(this.placeOfOrigin == 'NOT'){
+                alert('Zwariowałeś??? Transport z Alaski do Nowego Yorku? :):) Ja się tego nie podejmę!!!')
+            }else if(this.placeOfOrigin == 'NO'){
+                alert('Na Hawaje to sobie można pojechać a nie samochody przywozić')
+            }else return this.placeOfOrigin;
         },
         customTaxPoland: function () {
             return Math.round((+this.price + 800) * this.currencyRate * 0.1)
@@ -50,10 +54,38 @@ const app = new Vue({
     }
 });
 
-$(function() {
-    $.getJSON('https://api.fixer.io/latest?symbols=USD', function(data) {
-      var usdRate = data.rates.USD.toString();
-      var commaFormatted = usdRate.replace('.', ',')
-      document.getElementById('rate_usd').innerHTML = commaFormatted;
-    });
-  });
+for (let i = 2; i <= 5.01; i += 0.1){
+    const rateInLoop = document.getElementById('loopRate');
+    let option = document.createElement('option');
+    option.value = i;
+    option.innerHTML = Math.round(i * 10) / 10;
+    rateInLoop.options.add(option);
+};
+
+for (let i = 1000; i <= 100000; i = i+= 100){
+    const priceInLoop = document.getElementById('loopPrice');  
+    let option = document.createElement('option');
+    option.value = i;
+    option.innerHTML = i;
+    priceInLoop.options.add(option);
+};
+
+for (let i = 10000; i <= 500000; i = i+= 2000){
+    const marketPriceInLoop = document.getElementById('loopMarketPrice'); 
+    let option = document.createElement('option');
+    option.value = i;
+    option.innerHTML = i;
+    marketPriceInLoop.options.add(option);
+};
+
+// set endpoint and your access key
+endpoint = 'live'
+access_key = '618b84c46bd93d0253e2fa295a8ef4a';
+// get the most recent exchange rates via the "live" endpoint:
+$.ajax({
+    url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key,   
+    dataType: 'jsonp',
+    success: function(json) {
+        document.getElementById('rate').innerHTML = Math.round(json.quotes.USDPLN * 1000) / 1000;
+    }
+});
